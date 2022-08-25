f41d64edc7bc931805b35dae50c938808318edf1
Rodrigo Cisternas <rodrigocisternas@docmovi.com>
2022-08-25 15:58:39 +0200

    make api call to create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,32 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     render json: @product
   end
+
+  def create
+    @product = Product.create(
+      id: params[:id],
+      title: params[:product][:title],
+      description: params[:product][:description],
+      colors: params[:product][:colors],
+      price: params[:product][:price],
+      stock: params[:product][:stocks],
+      sizes: params[:product][:sizes],
+      images: params[:product][:images]
+    )
+    render json: @product
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(
+        :id,
+        :title,
+        :description,
+        :colors,
+        :price,
+        :stock,
+        :sizes,
+        :images
+      )
+    end
 end
